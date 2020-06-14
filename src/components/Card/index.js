949b07b9e1146d97be69592ee9b6e2a807832da6
Illia <kriminalmykriminal@gmail.com>
2020-06-14 19:33:58 +0300

    Components created

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Card.scss'
+
+
+const Card = _ => {
+	return (
+		<div>Пройти курс по react</div>
+	)
+}
+
+export default Card;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as Card} from './Card'
+export {default as Panel} from './Panel'
+export {default as Button} from './Button'
